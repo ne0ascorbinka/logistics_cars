ba59be2b58b603afe3bb4db665f628f8333bd55f
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-08-31 20:01:54 +0300

    include transport to INSTALLED_APPS

--- logistics_tracker/logistics_tracker/settings.py
+++ logistics_tracker/logistics_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transport' # or 'transport.apps.TransportConfig', but KISS, we have only 1 config
 ]
 
 MIDDLEWARE = [
